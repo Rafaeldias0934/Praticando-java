bd0c778445edae2bfe679aa389a0478ff380afd7
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-17 18:20:59 -0300

    Pratica Fundamentos

--- src/Fundamentos/OlaMundo.java
+++ src/Fundamentos/OlaMundo.java
@@ -3,5 +3,6 @@ package Fundamentos;
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Hello Word !!");
+        System.out.println("Funcionou!!");
     }
 }
