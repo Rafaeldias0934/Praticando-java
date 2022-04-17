6157dbc4c0a2c2fdf6b7abcf7bd6bb0b981bd264
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-17 18:26:26 -0300

    Pratica Fundamentos

---
+++ src/Fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package Fundamentos;
+
+public class PrimeiroPrograma {
+	public static void main(String[] args) {
+		// Uma sentença de código termina com ;
+		System.out.println("Primeiro Programa parte #01!");
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 */
+		System.out.println("Primeiro programa parte #02!");
+		System.out.println("Fim");
+	}
+}
