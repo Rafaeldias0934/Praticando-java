1d18e039c3e3fd3f0b6100c0cce154d6fcc43602
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-20 20:44:48 -0300

    Pratica Fundamentos

---
+++ src/Fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,12 @@
+package Fundamentos;
+
+public class PrimitivoVsObjeto {
+	public static void main(String[] args) {
+		//String s = new String("text");
+		//s.toUpperCase();
+		
+		// Wrappers são a versão dos tipos primitivos!
+		int a = 123;
+		System.out.println(a);
+	}
+}
