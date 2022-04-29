2a416e77e6385c20c568b750cd0d2881b057e55a
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-04-29 17:38:14 -0300

    Código não finalizado do Desafio.

---
+++ out/production/CourseJava/DesafioJogodaAdvinhacao/src/DesafioJogoDaAdvinhacao.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class DesafioJogoDaAdvinhacao {
+    public static void main(String[] args) {
+        /* Jogo da adivinhação: Tentar adivinhar um número entre 0 - 100.
+         Armazene um numero aleatório em uma variável. O Jogador tem 10
+         tentativas para adivinhar o número gerado. Ao final de cada tentativa,
+         imprima a quantidade de tentativas restantes, e imprima se o número
+         inserido é maior ou menor do que o número armazenado.
+        */
+        int numeroDigitado, qtdDeTentativas = 10;
+        Random gerador = new Random();
+        int numeroAleatorio = gerador.nextInt(100) + 1 ;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(numeroAleatorio);
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Digite um número para verificação");
+            numeroDigitado = scan.nextInt();
+
+            if (numeroDigitado > numeroAleatorio) {
+                System.out.println("O número digitado é maior, que o número sorteado.");
+            } else if (numeroDigitado < numeroAleatorio) {
+                System.out.println("O número digitado é menor, que o número sorteado.");
+            } else {
+                System.out.println("ACERTOU !! O número aleatório é " + numeroAleatorio);
+            }
+            qtdDeTentativas--;
+            System.out.printf("Agora você tem %d tentativas.\n",qtdDeTentativas);
+        }
+        System.out.println("Suas tentativas acabaram, Por favor, inicie o programa novamente.");    }
+}
