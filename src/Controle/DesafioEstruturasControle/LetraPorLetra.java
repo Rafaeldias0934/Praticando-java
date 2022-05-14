3cb4cdc10a901ccbf07c599be16b6c5cc6f12ba2
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-05-14 11:13:04 -0300

    Praticando Java

--- src/Controle/DesafioEstruturasControle/LetraPorLetra.java
+++ src/Controle/DesafioEstruturasControle/LetraPorLetra.java
@@ -1,5 +1,16 @@
 package Controle.DesafioEstruturasControle;
 
-public class LetraPorLetra {
+import java.util.Scanner;
 
+public class LetraPorLetra {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe a palavra desejada");
+        String s = scan.next();
+
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
+        }
+    }
 }
