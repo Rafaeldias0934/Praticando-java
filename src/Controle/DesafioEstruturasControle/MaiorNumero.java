456bc7e3aef5e0891fe8e8db8cd62abd94e865a0
Rafaeldias0934 <74202654+Rafaeldias0934@users.noreply.github.com>
2022-05-14 10:27:10 -0300

    Praticando Java

--- src/Controle/DesafioEstruturasControle/MaiorNumero.java
+++ src/Controle/DesafioEstruturasControle/MaiorNumero.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class MaiorNumero {
     public static void main(String[] args) {
-        int numeroDigitado,maior = 0,menor = 0;
+        int numeroDigitado,maior = 0;
         Scanner scan = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Informe o " + (i + 1) + " número");
             numeroDigitado = scan.nextInt();
             if (numeroDigitado > maior) {
